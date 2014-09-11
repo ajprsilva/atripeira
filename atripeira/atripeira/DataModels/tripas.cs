ffd159c8ea157f6a5acfae2100faaada6cdccbef
tomasek1414 <tomaseoliveira@live.com.pt>
2014-09-11 12:21:23 +0100

    fixes

---
+++ atripeira/atripeira/DataModels/tripas.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atripeira.DataModels
+{
+    public class tripas
+    {
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "historia")]
+        public string historia { get; set; }
+
+        [JsonProperty(PropertyName = "ingredientes")]
+        public string ingredientes { get; set; }
+
+        [JsonProperty(PropertyName = "receita")]
+        public string receita { get; set; }
+    }
+}
