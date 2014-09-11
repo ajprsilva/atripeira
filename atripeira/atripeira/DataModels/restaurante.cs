3e64184a5d3ec8a79459776e0026bb0d92e11df4
tomasek1414 <tomaseoliveira@live.com.pt>
2014-09-11 14:28:56 +0100

    new connection to restaurante

---
+++ atripeira/atripeira/DataModels/restaurante.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atripeira.DataModels
+{
+    public class restaurante
+    {
+
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "nome")]
+        public string nome { get; set; }
+        //public string morada { get; set; }
+
+        //public Image foto { get; set; }
+        [JsonProperty(PropertyName = "morada")]
+        public string morada { get; set; }
+
+        [JsonProperty(PropertyName = "Pontuacao")]
+        public string Pontuacao { get; set; }
+
+        [JsonProperty(PropertyName = "totalVotos")]
+        public int totalVotos { get; set; }
+    }
+}
