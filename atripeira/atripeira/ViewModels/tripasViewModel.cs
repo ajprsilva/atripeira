ceda49af311e73e1a133270a1b887575c871db8a
tomasek1414 <tomaseoliveira@live.com.pt>
2014-09-11 12:19:54 +0100

    fixes

---
+++ atripeira/atripeira/ViewModels/tripasViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using atripeira.Resources;
+using Microsoft.WindowsAzure.MobileServices;
+using atripeira.DataModels;
+
+namespace atripeira.ViewModels
+{
+    public partial class tripasViewModel : INotifyPropertyChanged
+    {
+        private MobileServiceCollection<tripas, tripas> items;
+        private IMobileServiceTable<tripas> todoTable = App.MobileService.GetTable<tripas>();
+
+        public tripasViewModel()
+        {
+            this.Items = new ObservableCollection<ItemViewModel>();
+        }
+
+        /// <summary>
+        /// A collection for ItemViewModel objects.
+        /// </summary>
+        public ObservableCollection<ItemViewModel> Items { get; private set; }
+
+        private string _sampleProperty = "Sample Runtime Property Value";
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding
+        /// </summary>
+        /// <returns></returns>
+        public string SampleProperty
+        {
+            get
+            {
+                return _sampleProperty;
+            }
+            set
+            {
+                if (value != _sampleProperty)
+                {
+                    _sampleProperty = value;
+                    NotifyPropertyChanged("SampleProperty");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sample property that returns a localized string
+        /// </summary>
+        public string LocalizedSampleProperty
+        {
+            get
+            {
+                return AppResources.SampleProperty;
+            }
+        }
+
+        public bool IsDataLoaded
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Creates and adds a few ItemViewModel objects into the Items collection.
+        /// </summary>
+        public async void LoadData()
+        {
+            try
+            {
+                items = await todoTable.CreateQuery().ToCollectionAsync();
+            }
+            catch (Exception ex) {
+                string st = ex.Message.ToString();
+            }            
+            
+            // Sample data; replace with real data
+            this.Items.Add(new ItemViewModel() { LineOne = items[0].historia, LineTwo = items[0].ingredientes, LineThree = items[0].receita });
+            this.IsDataLoaded = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
