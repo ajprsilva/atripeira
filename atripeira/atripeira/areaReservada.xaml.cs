d0dfc4390b786f74220a80850bd631bfcf5c672d
tomasek1414 <tomaseoliveira@live.com.pt>
2014-09-12 15:59:16 +0100

    Area Reservada e Manutençao - Graficos

---
+++ atripeira/atripeira/areaReservada.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace atripeira
+{
+    public partial class areaReservada : PhoneApplicationPage
+    {
+        public areaReservada()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplicationBarIconButton_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Manutencao.xaml", UriKind.Relative));
+        }
+    }
+}
