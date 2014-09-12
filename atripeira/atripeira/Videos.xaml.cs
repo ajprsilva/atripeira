4096cb6685724cec6285a480e061d483068ee849
tomasek1414 <tomaseoliveira@live.com.pt>
2014-09-12 13:53:32 +0100

    new design :D

--- atripeira/atripeira/Videos.xaml.cs
+++ atripeira/atripeira/Videos.xaml.cs
@@ -7,6 +7,8 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using MyToolkit.Multimedia;
+using MyToolkit.Networking;
 
 namespace atripeira
 {
@@ -15,6 +17,19 @@ namespace atripeira
         public Videos()
         {
             InitializeComponent();
+        }
+
+        public async void playYoutube(){
+
+            
+            await YouTube.PlayWithPageDeactivationAsync("LSj4OlGV4x0", true, YouTubeQuality.Quality480P);
+            
+            
+        }
+
+        private void ApplicationBarIconButton_Click(object sender, EventArgs e)
+        {
+            playYoutube();
         }
     }
 }
