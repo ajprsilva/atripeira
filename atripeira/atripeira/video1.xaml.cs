818c58cd4d93c46b781e2265bdc0ac5a53d2e51b
tomasek <1130154@isep.ipp.pt>
2014-09-14 04:10:12 +0100

    amo te *.*

--- atripeira/atripeira/video1.xaml.cs
+++ atripeira/atripeira/video1.xaml.cs
@@ -7,14 +7,18 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using atripeira.ViewModels;
 
 namespace atripeira
 {
     public partial class video1 : PhoneApplicationPage
     {
+        private videoViewModel TripasViewModel = new videoViewModel();
         public video1()
         {
             InitializeComponent();
+            DataContext = TripasViewModel;
+            TripasViewModel.LoadData();
         }
     }
 }
