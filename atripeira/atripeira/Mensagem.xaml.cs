40bd2b1292d601fb52d9e8d36823aceee28266fe
Diogo Silva <1110499@isep.ipp.pt>
2014-09-14 06:00:00 +0100

    Mensagem

---
+++ atripeira/atripeira/Mensagem.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.Tasks;
+
+namespace atripeira
+{
+    public partial class Mensagem : PhoneApplicationPage
+    {
+        public Mensagem()
+        {
+            InitializeComponent();
+        }
+
+        private void SendMensagem_Click(object sender, EventArgs e)
+        {
+            SmsComposeTask smsComposeTask = new SmsComposeTask();
+
+            string texto="";
+
+            texto = "O Cliente " + nome.Text + " efectuou uma reserva com " + num.Text + " pessoas para as " + hora.Text + " horas";
+
+            smsComposeTask.To = "+351919856385";
+            smsComposeTask.Body = texto;
+
+            smsComposeTask.Show();
+
+            if (this.NavigationService.CanGoBack)
+            {
+                this.NavigationService.GoBack();
+            }
+
+        }
+    }
+}
